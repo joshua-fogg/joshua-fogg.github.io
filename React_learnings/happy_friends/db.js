153679bd61d4df2a3fd9026c15ef7892c3d9766a
joshua-fogg <joshuafoggnz@gmail.com>
2017-07-25 18:59:31 +1200

    added remainder

---
+++ React_learnings/happy_friends/db.js
@@ -0,0 +1,18 @@
+// const fs = require('fs')
+
+// function getData(cb) {
+//   fs.readFile('./data.json', (err, puppyData) => {
+//     if (err) {
+//       cb(err)
+//       return
+//     }
+//     const objPuppy = JSON.parse(puppyData)
+//     cb(null, objPuppy)
+//   })
+// }
+
+// module.exports = {
+//   getData: getData,
+//   saveData: saveData
+// }
+
