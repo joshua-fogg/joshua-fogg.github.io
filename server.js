7ce2054c0e39250660047b7c280841d1bc0df8d6
Joshua-Fogg <joshuafoggnz@gmail.com>
2017-10-03 11:11:37 +1300

    update server && add react packages

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 var express = require('express')
-
-var PORT = process.env.PORT || 3000
 var server = express()
+
+var App = require('')
+var PORT = 3000
+
 
 server.use(express.static('public'))
 
 server.listen(PORT, function () {
   console.log('Listening on port', PORT)
+})
+
+server.get('/*', (req, res) => {
+  res.send(App)
 })
