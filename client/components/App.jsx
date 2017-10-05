316b91889cf65d3ff4b95cc1c9367a7ae1b886c9
Joshua-Fogg <joshuafoggnz@gmail.com>
2017-10-05 14:25:49 +1300

    switch to Hash Router

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Switch, BrowserRouter as Router } from 'react-router-dom'
+import { Route, Switch, HashRouter as Router } from 'react-router-dom'
 import { render, ReactDOM } from 'react-dom'
 
 // component imports go here
