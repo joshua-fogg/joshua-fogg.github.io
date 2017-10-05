f01ac92c97bc446f96eabff7049154f8d80b2aa7
Joshua-Fogg <joshuafoggnz@gmail.com>
2017-10-05 14:13:52 +1300

    add Nav Bar Routing

---
+++ client/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router'
+
+const Header = () => (
+  <div className="header">
+    <div id="name"><h1> Joshua Fogg </h1> Comming Up For Air </div>
+    <div id="navigation-bar">
+      <Link to='/blogs'>Blog</Link>
+      <Link to='/about'>About</Link>
+      <Link to='/portfolio'>Portfolio </Link>
+    </div>
+  </div>
+)
