c1474e7ca77c14c64ef4a8c8c07b2f21cf03bb5d
Joshua-Fogg <joshuafoggnz@gmail.com>
2017-10-10 12:32:24 +1300

    add styling to map function

--- client/components/Blogs.jsx
+++ client/components/Blogs.jsx
@@ -19,7 +19,20 @@ class Blogs extends React.Component {
         <h3>Blog Categories:</h3>
         <ol>
         {this.state.Content.map((category, i)=>{
-          return <li key={i}>{category[0].name}</li>
+          const style = {
+            width: '70%',
+            height: '300px',
+            border: 'solid 2px grey',
+            color: 'green',
+            backgroundImage: "url(" + category[0].image + ")"
+          }
+          return (
+            <li key={i}> 
+              <div style={style}>
+              <h4>{category[0].name}:</h4>  
+              </div>
+            </li>
+            )
         })}
         </ol>
       </div>
