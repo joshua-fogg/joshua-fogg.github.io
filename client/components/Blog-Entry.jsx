4ded71cf2794b256fe7ef95d1573fe0992a0db44
Joshua-Fogg <joshuafoggnz@gmail.com>
2017-10-10 15:11:17 +1300

    create blog entry outline

--- client/components/Blog-Entry.jsx
+++ client/components/Blog-Entry.jsx
@@ -2,9 +2,28 @@ import React from 'react'
 import { render } from 'react-dom'
 
 const Blog = (props) => {
+  const entry = props.entry
+  const lastEntry = entry.length()
+  const images = props.entry.length(-1)
+  console.log('images:',images, 'LastEntry:',lastEntry)
   return (
-    <div>
+    <div className="">
+      <h1>{entry["page-title"]}</h1>
+      {entry.content.map((contentPartial, i) => {
+        let CP = contentPartial
+        return (
+          <div className="partial" key={i}>
+            <h2>{CP.heading}</h2>
+            <p>{CP.content}</p>
+          </div>
+        )
+      })
+      }
+      {entry.content.length(-1)}
+      <div className="">
 
+      </div>
     </div>
   )
 }
+export default Blog
