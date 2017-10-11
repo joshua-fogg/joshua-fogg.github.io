fbd5441746a7e4caebc52eb769f85f7b70504c2b
Joshua-Fogg <joshuafoggnz@gmail.com>
2017-10-11 14:54:08 +1300

    add fotter

---
+++ client/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link, withRouter } from 'react-router-dom'
+
+const Header = (props) => {
+  return (
+    <div className="footer">
+      FOOTER
+    </div>
+  )
+}
+
+export default Header
