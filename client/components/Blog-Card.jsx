5278c8022c09a8dab4842dc77a3f632e90d28781
Joshua-Fogg <joshuafoggnz@gmail.com>
2017-10-11 14:54:55 +1300

    rename const's

--- client/components/Blog-Card.jsx
+++ client/components/Blog-Card.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 const card = (props) => {
   console.log(props)
-  const CP = props.contentPartial
+  const Blog = props.content
   return (
     <div className="card" key={props.i}>
       <div className="card-image">
         <img src="" alt="" />
       </div>
       <div>
-        <h2>{CP.heading}</h2>
-        <p>{CP.content}</p>
+        <h2>{Blog.heading}</h2>
+        <p>{Blog.content}</p>
       </div>
     </div>
   )
