c5c5ed691033a40f4d310f8502ae4bdf35eae2c2
Joshua-Fogg <joshuafoggnz@gmail.com>
2017-10-11 13:54:33 +1300

    add img tag

--- client/components/Blog-Card.jsx
+++ client/components/Blog-Card.jsx
@@ -4,9 +4,15 @@ const card = (props) => {
   console.log(props)
   const CP = props.contentPartial
   return (
-    <div className="partial" key={props.i}>
-      <h2>{CP.heading}</h2>
-      <p>{CP.content}</p>
+    <div className="card" key={props.i}>
+      <div className="card-image">
+        <img src="" alt="" />
+      </div>
+      <div>
+        <h2>{CP.heading}</h2>
+        <p>{CP.content}</p>
+      </div>
+      
     </div>
   )
 }
