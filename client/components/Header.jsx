b01d74f5e0f22fb528692e618214823c98a0b11e
Joshua-Fogg <joshuafoggnz@gmail.com>
2017-10-11 14:54:18 +1300

    correct Header

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, withRouter } from 'react-router-dom'
 
-const Header = () => (
-  <div className="header">
-    <div id="name"><h1> Joshua Fogg </h1> Comming Up For Air </div>
-    <div id="navigation-bar">
-      <Link to='/blogs'>Blog</Link>
-      <Link to='/about'>About</Link>
-      <Link to='/portfolio'>Portfolio </Link>
+const Header = (props) => {
+  return (
+    <div className="header">
+      <div id="name"><h1> Joshua Fogg </h1> Comming Up For Air </div>
+      <div id="navigation-bar">
+        <Link to='/blogs'>Blog</Link>
+        <Link to='/about'>About</Link>
+        <Link to='/portfolio'>Portfolio </Link>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default Header
